87892c82ee36b03f40b2d4b90a36c79d79038267
orauline <orauline@student.42perpignan.fr>
2024-01-29 12:45:07 +0100

    cub_utils modifié

--- src/cub_utils.c
+++ src/cub_utils.c
@@ -6,48 +6,6 @@ void	ft_error(char *str)
 	ft_printf("Error :\n%s\n", str);
 	exit(EXIT_FAILURE);
 }
-/*Fonction qui free les tableaux précédemment malloc en cas d'erreur*/
-void	free_errors(t_game *game)
-{
-	int	i;
-
-	i = -1;
-	while (game->tab[++i])
-		free(game->tab[i]);
-	free(game->tab);
-	i = -1;
-	while (game->config[++i])
-		free(game->config[i]);
-	free(game->config);
-	i = -1;
-	while (game->matrix[++i])
-		free(game->matrix[i]);
-	free(game->matrix);
-}
-/*Fonction qui free les tableaux + indique le message d'erreur correspondant*/
-void	free_errors2(t_game *game, char *str)
-{
-	free_errors(game);
-	ft_error(str);
-}
-/*Fonction qui free tout en fin de programme*/
-void	free_all(t_game *game)
-{
-	int	i;
-
-	i = -1;
-	while (game->tab[++i])
-		free(game->tab[i]);
-	free(game->tab);
-	i = -1;
-	while (game->config[++i])
-		free(game->config[i]);
-	free(game->config);
-	i = -1;
-	while (game->matrix[++i])
-		free(game->matrix[i]);
-	free(game->matrix);
-}
 
 /*Fonction qui récupère la valeur de la ligne la plus longue*/
 void	get_max_line_len(t_game *game)
